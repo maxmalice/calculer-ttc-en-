3c4ff9102e9a683f29aaaffc64435448c844cf0d
maxime <44111812+maxmalice@users.noreply.github.com>
2023-06-22 14:53:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var variable1 = parseFloat(prompt("Entrez la valeur de la variable 1 :"));
+var variable2 = parseFloat(prompt("Entrez la valeur de la variable 2 :"));
+var somme = variable1 + variable2;
+alert("La somme est : " + somme);
